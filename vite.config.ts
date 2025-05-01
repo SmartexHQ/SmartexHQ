e2d2f578211fcfd29aaffb32465a2446100d581d
Smart  Raysam <engsamtob@yahoo.com>
2025-05-01 04:31:33 +0100

    fix: correct base URL in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: 'https://smartexg.com/',
+  base: 'https://smartexng.com/',
   server: {
     host: "::",
     port: 8080,
